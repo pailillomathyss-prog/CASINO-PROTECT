d6f9b94755222f4cd752012824ae346224134fd4
pailillomathyss-prog <pailillomathyss@gmail.com>
2026-06-12 03:54:43 +0200

    Set CLIENT_ID=1514788470961475636, fallback to global slash deploy if no GUILD_ID

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@ require('dotenv').config();
 
 module.exports = {
   TOKEN: process.env.DISCORD_TOKEN,
-  CLIENT_ID: process.env.CLIENT_ID,
-  GUILD_ID: process.env.GUILD_ID,
-  LOGS_CHANNEL_ID: process.env.LOGS_CHANNEL_ID,
-  TICKET_CHANNEL_ID: process.env.TICKET_CHANNEL_ID,
-  TICKET_CATEGORY_ID: process.env.TICKET_CATEGORY_ID,
-  REGLEMENT_CHANNEL_ID: process.env.REGLEMENT_CHANNEL_ID,
-  VERIFIED_ROLE_ID: process.env.VERIFIED_ROLE_ID,
-  STAFF_ROLE_ID: process.env.STAFF_ROLE_ID,
+  CLIENT_ID: process.env.CLIENT_ID || '1514788470961475636',
+  GUILD_ID: process.env.GUILD_ID || null,
+  LOGS_CHANNEL_ID: process.env.LOGS_CHANNEL_ID || null,
+  TICKET_CHANNEL_ID: process.env.TICKET_CHANNEL_ID || null,
+  TICKET_CATEGORY_ID: process.env.TICKET_CATEGORY_ID || null,
+  REGLEMENT_CHANNEL_ID: process.env.REGLEMENT_CHANNEL_ID || null,
+  VERIFIED_ROLE_ID: process.env.VERIFIED_ROLE_ID || null,
+  STAFF_ROLE_ID: process.env.STAFF_ROLE_ID || null,
 };
